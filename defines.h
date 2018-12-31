1a6c2e7b225de51a8918760a4ff9122def85846c
Yang Yang <yyang.even@gmail.com>
2018-12-31 12:43:30 -0800

    Code: Add set_debug_level
    This first program is based on the code of
    [my other repository](https://github.com/yyang-even/yyangtech_wordpress_com/tree/master/2018_12_27).
    
    Change-Id: I7d300e31bd4a6294e7d49cb18d29c6ed0aeafedb

---
+++ defines.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "ldap_connection.h"
+
+const std::string URI = "ldap://";
+const std::string DEFAULT_BASE = "dc=yyangtech,dc=wordpress,dc=com";
+const auto DEFAULT_SCOPE = LDAP_SCOPE_BASE;
