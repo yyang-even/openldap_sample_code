bcb0798104f25c7d84c5f218e19ec8578a245714
Yang Yang <yyang.even@gmail.com>
2019-01-02 16:51:10 -0800

    Code: Search memberof attribute
    Change-Id: Ieecb0189bffc034012487aae8f1d9ff871ce0127

--- synch_search_with_attributes.cpp
+++ synch_search_with_attributes.cpp
@@ -4,7 +4,8 @@ int main(int argc, char **argv) {
     use_ldap_version3();
     LdapConnection ldap = {URI};
 
-    AttributesArray<2> attributes = {"dn", "sn"};
+    AttributesArray<2> attributes = {"sn", "memberof"};
 
-    ldap.SynchSearchAndPrint(DEFAULT_BASE, DEFAULT_SCOPE, "", attributes.C_StrArray());
+    ldap.SynchSearchAndPrint(DEFAULT_BASE, DEFAULT_SCOPE, "objectClass=inetOrgPerson",
+                             attributes.C_StrArray());
 }
